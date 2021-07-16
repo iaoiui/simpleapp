115b0cd2eb39536a8ed7d9950768bc76090ee29d
mita <iaoiui0727@gmail.com>
2021-07-16 16:27:24 +0900

    remove test

--- simpleapp_test.go
+++ simpleapp_test.go
@@ -1,11 +1,7 @@
 package simpleapp
 
 import (
-	"log"
 	"os"
-	"os/user"
-	"path/filepath"
-	"reflect"
 	"strings"
 	"testing"
 )
@@ -22,32 +18,32 @@ func Test_main(t *testing.T) {
 	}
 }
 
-func TestReadCSV(t *testing.T) {
-	type args struct {
-		filepath string
-	}
-	user, err := user.Current()
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-	homeDirectory := user.HomeDir
-	tests := []struct {
-		name string
-		args args
-		want CSV
-	}{
-		{"read csv",
-			args{filepath.Join(homeDirectory, "test.csv")},
-			CSV{[][]string{{"name", "age"}, {"Alice", "28"}, {"Bob", "29"}}}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got, _ := ReadCSV(tt.args.filepath); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ReadCSV() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+//func TestReadCSV(t *testing.T) {
+//	type args struct {
+//		filepath string
+//	}
+//	user, err := user.Current()
+//	if err != nil {
+//		log.Fatalf(err.Error())
+//	}
+//	homeDirectory := user.HomeDir
+//	tests := []struct {
+//		name string
+//		args args
+//		want CSV
+//	}{
+//		{"read csv",
+//			args{filepath.Join(homeDirectory, "test.csv")},
+//			CSV{[][]string{{"name", "age"}, {"Alice", "28"}, {"Bob", "29"}}}},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			if got, _ := ReadCSV(tt.args.filepath); !reflect.DeepEqual(got, tt.want) {
+//				t.Errorf("ReadCSV() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
 
 func TestEnv(t *testing.T) {
 	os.Setenv("TEST", "test")
